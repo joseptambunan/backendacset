f800bf8e698e3f4e0fe03208ad1dd6fc4e80c04a
Josep Tambunan <josep@narrada.com>
2017-09-20 14:47:16 +0700

    add public view message

---
+++ public/views/message_from_ceo_id.php
@@ -0,0 +1,12 @@
+<!-- <div class="image-wrapper">
+	<img src="<?= base_url($img_msc) ?>" class="img-responsive">
+</div> -->
+
+<div class="subtitle-about">
+	<h4>PESAN PRESIDEN DIREKTUR</h4>
+</div>
+
+<div class="about-content">
+	<img src="<?= base_url($img_msc); ?>" alt="structure" class="img-responsive">
+	<?= $content_msc ?>
+</div>
